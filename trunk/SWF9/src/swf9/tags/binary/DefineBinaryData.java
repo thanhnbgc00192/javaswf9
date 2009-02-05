9393d7f35cc52b54be124b3e18dea66409ae00ba
stefano.cottafavi <stefano.cottafavi@6040655a-f1ab-11dd-99c0-75d572ba1ddd>
2009-02-05 02:25:32 +0000

    Added Ant builder

--- trunk/SWF9/src/swf9/tags/binary/DefineBinaryData.java
+++ trunk/SWF9/src/swf9/tags/binary/DefineBinaryData.java
@@ -1,4 +1,15 @@
 package swf9.tags.binary;
+/**
+ * Copyright (c) 2008  Stefano Cottafavi and .:: A Quarter to Seven ::.
+ * 
+ * Some rights reserved.
+ * 
+ * Licensed under the CREATIVE COMMONS Attribution-Noncommercial-Share Alike 3.0
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at: http://creativecommons.org/licenses/by-nc-sa/3.0/us/
+ * 
+ * Please visit http://www.stefanocottafavi.com for more information
+**/
 
 import swf9.tags.Tag;
 
